e9b6a1c2068efc36c274336d91f5de61d4fc85ef
phienkv <phien@planday.com>
2015-01-01 17:08:15 +0700

    no message

--- models/Item.js
+++ models/Item.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 var itemSchema = new mongoose.Schema({
+    _id: String,
     word: {type: String, lowercase: true},
     listId: String,
     createdDate: Date
